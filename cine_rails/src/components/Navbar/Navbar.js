8c2ab2f1d55ff26bc73159addfa7a9963fff9a72
Luiz Guilherme de Bem <luizdebem99@gmail.com>
2021-03-12 00:35:18 -0300

    Navbar current page fix

--- cine_rails/src/components/Navbar/Navbar.js
+++ cine_rails/src/components/Navbar/Navbar.js
@@ -1,22 +1,25 @@
 import React from 'react'
-import { Navbar as NavbarBootstrap, Nav, Form, FormControl, Button } from 'react-bootstrap';
+import { Navbar as NavbarBootstrap, Nav, Form, FormControl, Button, Row } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
+import { withRouter } from 'react-router';
+import './Navbar.css';
 
-const Navbar = () => {
+const Navbar = ({ location }) => {
+  console.log(location);
   return (
     <>
       <NavbarBootstrap bg="dark" variant="dark">
         <NavbarBootstrap.Brand>
           <h1>Cine Rails</h1>
         </NavbarBootstrap.Brand>
-        <Nav className="mr-auto">
+        <Row className="mr-auto ml-2">
           <LinkContainer to="/">
-            <Nav.Link>Todos os filmes</Nav.Link>
+            <Nav.Link className={location.pathname === '/' ? 'nav-link current' : 'nav-link'}>Todos os filmes</Nav.Link>
           </LinkContainer>
           <LinkContainer to="/form">
-            <Nav.Link>Adicionar um filme</Nav.Link>
+            <Nav.Link className={location.pathname === '/form' ? 'nav-link current' : 'nav-link'}>Adicionar um filme</Nav.Link>
           </LinkContainer>
-        </Nav>
+        </Row>
         <Form inline>
           <FormControl type="text" placeholder="Search" className="mr-sm-2" />
           <Button variant="outline-info">Search</Button>
@@ -26,4 +29,4 @@ const Navbar = () => {
   )
 }
 
-export default Navbar
+export default withRouter(Navbar)
